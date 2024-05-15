1ded6e115990e5dfba1ef9cf49eb9c7e1dd7a630
hamed mohsen <raahnaward@gmail.com>
2024-05-15 16:43:07 +0200

    final commit

--- vite-project/src/components/NewestRecipes.jsx
+++ vite-project/src/components/NewestRecipes.jsx
@@ -16,34 +16,34 @@ const NewestRecipes = () => {
             <div className="grid lg:grid-cols-4 gap-6">
                 <div className="bg-orange-100 my-10 shadow-md rounded overflow-hidden">
                     <a href="./Recipe2.html"><img className="w-full h-32 sm:h-64 object-cover" src={lachs1} alt="Lachs"/></a>
-                    <h3 className="my-5 pl-3 text-md font-bold">Steamed Salmon</h3>
+                    <h3 className="my-5 pl-3 text-md font-bold font-serif">Steamed Salmon</h3>
                         <div className="pl-3 flex">
                             <img className="rounded-full size-12" src={tio1} alt="denis"/>
-                            <h3 className="my-5 pl-3 text-md font-bold">Denis</h3>
+                            <h3 className="my-5 pl-3 text-md font-semibold ">Denis</h3>
                         </div>
                 </div>
                 <div className="bg-orange-100 my-10 shadow-md rounded overflow-hidden">
                     <a href="./Recipe3.html"><img className="w-full h-32 sm:h-64 object-cover" src={masala1} alt="masala"/></a>
-                    <h3 className="my-5 pl-3 text-md font-bold">Chicken Masala</h3>
+                    <h3 className="my-5 pl-3 text-md font-bold font-serif">Chicken Masala</h3>
                         <div className="pl-3 flex">
                             <img className="rounded-full size-12" src={tia2} alt="anitha"/>
-                            <h3 className="my-5 pl-3 text-md font-bold">Anitha</h3>
+                            <h3 className="my-5 pl-3 text-md font-semibold">Anitha</h3>
                         </div>
                 </div>
                 <div className="bg-orange-100 my-10 shadow-md rounded overflow-hidden">
                     <a href="./Recipe4.html"><img className="w-full h-32 sm:h-64 object-cover" src={rabbit2} alt="rabbit"/></a>
-                    <h3 className="my-5 pl-3 text-md font-bold">Rabbit Stew</h3>
+                    <h3 className="my-5 pl-3 text-md font-bold font-serif">Rabbit Stew</h3>
                         <div className="pl-3 flex">
                             <img className="rounded-full size-12" src={tio2} alt="yago"/>
-                            <h3 className="my-5 pl-3 text-md font-bold">Yago</h3>
+                            <h3 className="my-5 pl-3 text-md font-semibold">Yago</h3>
                         </div>
                 </div>
                 <div className="bg-orange-100 my-10 shadow-md rounded overflow-hidden">
                     <a href="./Recipe1.html"><img className="w-full h-32 sm:h-64 object-cover" src={Vanilla} alt="vanilla"/></a>
-                    <h3 className="my-5 pl-3 text-md font-bold">Vanilla Cake</h3>
+                    <h3 className="my-5 pl-3 text-md font-bold font-serif">Vanilla Cake</h3>
                         <div className="pl-3 flex">
                             <img className="rounded-full size-12" src={tia1} alt="rabia"/>
-                            <h3 className="my-3 pl-3 text-md font-bold">Rabia</h3>
+                            <h3 className="my-3 pl-3 text-md font-semibold">Rabia</h3>
                         </div>
                 </div>
             </div>
