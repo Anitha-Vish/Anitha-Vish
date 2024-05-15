ce455e98c8e646c7c3909cf648edc35f64b77e26
Anitha <anithamca.k2008@gmail.com>
2024-05-15 16:53:28 +0200

    Fetch Data

--- vite-project/src/components/Newrecipe.jsx
+++ vite-project/src/components/Newrecipe.jsx
@@ -1,21 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { createClient } from "contentful";
-import "./App.css";
+import "../App.css";
 
 const Newrecipe = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
     const client = createClient({
-      space: process.env.REACT_APP_CONTENTFUL_SPACE_ID,
-      accessToken: process.env.REACT_APP_CONTENTFUL_ACCESS_TOKEN,
+      space: "6egxy69oc347",
+      accessToken: "M_1VuSl7HCwwCXh21aGsoBGa7e1oZi8f7LhDp-7rI0A",
     });
 
     const fetchData = async () => {
       try {
-        const response = await client.getEntries
-        ({ content_type: 'cookBook' });
-      
+        const response = await client.getEntries({ content_type: "cookBook" });
+        console.log(response.items);
         setData(response.items);
       } catch (error) {
         console.error("Error fetching data from Contentful:", error);
@@ -31,16 +30,36 @@ const Newrecipe = () => {
 
   return (
     <div>
-      <h1>Contentful Data</h1>
-      {data ? (
-        <ul>
-          {data.map((item) => (
-            <li key={item.sys.id}>{item.fields.title}</li>
-          ))}
-        </ul>
-      ) : (
-        <p>Loading...</p>
-      )}
+      <div>
+        <h4 className='text-grey-700 text-4xl mt-8 font-serif'>New Recipes </h4>
+        <hr className='border-orange-500 border-1 mt-2' />
+      </div>
+
+      <div className='mt-8 grid lg:grid-cols-4 gap-6 '>
+        {data.map((item) => (
+          <div
+            key={item.sys.id}
+            className='bg-white rounded overflow-hidden shadow-md relative'>
+            <div>
+              <img
+                src={item.fields.featuredImage.fields.file.url}
+           
+                className='w-full h-32 sm:h-64 object-cover '
+              />
+              <div className='my-4 px-2'>
+                <a href='/'>
+                  <span className='font-bold no-underline hover:underline font-serif'>
+                    {item.fields.name}
+                  </span>
+                  <br />
+                </a>
+                <span className='text-xs'>{item.fields.description}</span>
+                <br />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
